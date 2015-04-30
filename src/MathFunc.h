aa761cadeee3b3a5df85765ae9a02849369de45c
Joven Agravante <donjoven.agravante@lirmm.fr>
2015-04-30 17:35:51 +0200

    correct some comment grammar

--- src/MathFunc.h
+++ src/MathFunc.h
@@ -29,15 +29,15 @@ T sinc_inv(const T x)
 	const T taylor_2_bound = std::sqrt(taylor_0_bound);
 	const T taylor_n_bound = std::sqrt(taylor_2_bound);
 
-	// We use the 4 order taylor series around 0 of x/sin(x) to compute
+	// We use the 4th order taylor series around 0 of x/sin(x) to compute
 	// this function:
 	//      2      4
 	//     x    7⋅x     ⎛ 6⎞
 	// 1 + ── + ──── + O⎝x ⎠
 	//     6    360
-	// this approximation is valide around 0.
-	// if x is far away 0, our approximation is no more valid
-	// since x^6 become non negligable we use the normal computation of the function
+	// this approximation is valid around 0.
+	// if x is far from 0, our approximation is not valid
+	// since x^6 becomes non negligable we use the normal computation of the function
 	// (i.e. taylor_2_bound^6 + taylor_0_bound == taylor_0_bound but
 	//       taylor_n_bound^6 + taylor_0_bound != taylor_0).
 
@@ -48,13 +48,13 @@ T sinc_inv(const T x)
 	else
 	{
 		// x is bellow taylor_n_bound so we don't care of the 6th order term of
-		// the taylor serie.
+		// the taylor series.
 		// We set the 0 order term.
 		T result = static_cast<T>(1);
 
 		if(std::abs(x) >= taylor_0_bound)
 		{
-			// x is upper the machine epsilon so x^2 is meaningful.
+			// x is above the machine epsilon so x^2 is meaningful.
 			T x2 = x*x;
 			result += x2/static_cast<T>(6);
 

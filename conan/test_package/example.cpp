be46b8eb71452cd0430f762e2479d71ea64f90e0
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-04-02 15:05:40 +0900

    [conan] Add conan support

---
+++ conan/test_package/example.cpp
@@ -0,0 +1,9 @@
+#include <SpaceVecAlg/SpaceVecAlg>
+
+#include <iostream>
+
+int main()
+{
+  std::cout << sva::PTransformd::Identity() << "\n";
+  return 0;
+}
---
+++ conan/test_package/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sva
+print("PTransformd.Identity():\n{}".format(sva.PTransformd.Identity()))
